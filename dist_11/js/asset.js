aeaa638f4acabc6d045ed17765ee2e7efcdd9a4a
Aleksey <61293074+traycer255@users.noreply.github.com>
2021-03-19 11:30:49 +0500

    add modal window in MK site

--- dist_11/js/asset.js
+++ dist_11/js/asset.js
@@ -1,8 +1,24 @@
 $(document).ready(function() {
   $('#app').vide({
-    mp4: '../dist_11/video/video.mp4',
-    webm: '../dist_11/video/video.webm',
-    ogv: '../dist_11/video/video.ogv',
-    poster: '../dist_11/img/cover-image.jpg'
+    mp4: '../video/video.mp4',
+    webm: '../video/video.webm',
+    ogv: '../video/video.ogv',
+    poster: '../img/cover-image.jpg'
   })
+});
+
+$(document).ready(function() {
+	var button = $('#button-primery');
+	var modal = $('#modal');
+	var close = $('#close');
+
+	console.log(button);
+
+	button.on('click', function() {
+		modal.addClass('modal_active');
+	});
+
+	close.on('click', function() {
+		modal.removeClass('modal_active');
+	});
 });
