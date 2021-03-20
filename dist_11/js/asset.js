b535adfd39af10dc6683d26168a8e35d7561c0e4
Aleksey <61293074+traycer255@users.noreply.github.com>
2021-03-21 00:23:35 +0500

    change ways to video

--- dist_11/js/asset.js
+++ dist_11/js/asset.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   $('#app').vide({
-    mp4: '../video/video.mp4',
-    webm: '../video/video.webm',
-    ogv: '../video/video.ogv'
+    mp4: '../dist_11/video/video.mp4',
+    webm: '../dist_11//video/video.webm',
+    ogv: '../dist_11//video/video.ogv'
     // poster: '../img/cover-image.jpg'
   })
 });
