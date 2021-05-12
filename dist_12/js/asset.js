cc997550d3175f66d91b6c31bf20c4057c4291a8
Aleksey <61293074+traycer255@users.noreply.github.com>
2021-05-12 07:38:42 +0500

    Добавил сайт "Белаз" без скриптов

---
+++ dist_12/js/asset.js
@@ -0,0 +1,17 @@
+$('.header__button').magnificPopup({
+  type: 'inline'
+  // other options
+})
+
+
+
+var out = 55
+
+function myTest(x,y) {
+	var name = 25
+
+	return x * y
+}
+
+var res = myTest(25,8)
+console.log(res)
